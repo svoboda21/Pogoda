1222688a2e635e23c50c2516160006e7c043ca97
svoboda21 <39490042+svoboda21@users.noreply.github.com>
2018-06-03 18:18:20 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $file = "http://api.openweathermap.org/data/2.5/weather?id=524901&lang=ru&units=metric&APPID=39679d58e311e94a32d72cb553735dcc";
+    $string=file_get_contents ($file);
+    file_put_contents("c:\prog\m.txt", $string);
+    $pogoda = json_decode($string);
+    $temp=$pogoda->main->temp;
+    $davl=$pogoda->main->pressure;
+    $osad=$pogoda->weather[0]->description;
+    $icon=$pogoda->weather[0]->icon.".png";
+    $city = $pogoda->name;
+    $date = date("F j, Y");
+    echo $city . " - " .$date . "<br>";
+    echo "Температура: " . $temp ."&deg;C<br>";
+    echo "Осадки: " . $osad ." <br>";
+    echo "Давление: " . $davl ." мм рт ст <br>";
+    echo "<img src='http://openweathermap.org/img/w/" . $icon ."'/ >";
